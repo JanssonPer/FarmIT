14671771dda34a14b7653741db0596e1242fb728
VIKTOR BRUHN <sys14vbr@lu.se>
2016-03-29 17:58:39 +0200

    hej

---
+++ FarmITApp/FarmITApp/Model/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmITApp.Model
+{
+    public abstract class Animal
+    {
+        private long idAnimal;
+        private String name;
+        private String age;
+        private String statusAnimal;
+        private Box box;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public string StatusAnimal
+        {
+            get
+            {
+                return statusAnimal;
+            }
+            set
+            {
+                statusAnimal = value;
+            }
+        }
+        public Box 
+        {
+            get
+            {
+                return box;
+            }
+    set
+            {
+                box = value;
+            }
+        }
+
+    }
+}
