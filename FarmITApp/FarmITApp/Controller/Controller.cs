0d972e16ab8578aa59429a77019a5c31aa4bdfa0
Per Jansson <perviktorjansson@gmail.com>
2016-04-04 13:33:24 +0200

    real kontrollant

--- FarmITApp/FarmITApp/Controller/Controller.cs
+++ FarmITApp/FarmITApp/Controller/Controller.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FarmITApp.EntityFramework;
+using FarmITApp.DatabaseAccessLayer;
 
 namespace FarmITApp.Controller
 {
-    class Controller
+    public class Controller
     {
+        Dal dal = new Dal();
+
+        //Get methods
+        public Animals GetAnimal(String id)
+        {
+            try
+            {
+                long id1 = ((long)(System.Convert.ChangeType(id, typeof(long))));
+                return dal.GetAnimal(id1);
+            }
+            catch
+            {
+                //Exception
+                return null;
+            }
+        }
+        
     }
 }
